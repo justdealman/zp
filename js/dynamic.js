26e5c66c3bf87f0d8f17d085da60cf380ee01cf5
Alexandr <justdealman@gmail.com>
2015-08-14 17:38:21 +0700

    original

---
+++ js/dynamic.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function() {
+	if ( $('.slider').length > 0 ) {
+		$('.slider .core > div > div').slides({
+			generatePagination: true,
+			generateNextPrev: true,
+			container: 'container',
+			effect: 'fade',
+			fadeSpeed: 200,
+			play: 10000,
+			pause: 2500,
+		});
+	}
+});
